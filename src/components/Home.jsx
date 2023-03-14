16e5cf66cab49bcf8a24b94e679ec74de7dabaa2
Atikur Rahaman <atikurahad@gmail.com>
2023-03-15 00:38:04 +0600

    updating some components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-       <div><img src="https://chaldn.com/asset/Egg.ChaldalWeb.Fabric/Egg.ChaldalWeb1/1.0.0+Deploy-Release-227/Default/stores/chaldal/components/landingPage2/LandingPage/images/landingBannerTop.png" alt="" /></div>
+       <div >
+        <p className='bg-[url(https://chaldn.com/asset/Egg.ChaldalWeb.Fabric/Egg.ChaldalWeb1/1.0.0+Deploy-Release-227/Default/stores/chaldal/components/landingPage2/LandingPage/images/landingBannerTop.png)] h-[20rem]'>Get and grab your offer</p>
+        <img src="" alt="" className='mx-auto m-2'  /></div>
     </div>
   );
 };
