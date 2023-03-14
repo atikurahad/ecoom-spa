537e83eadfcddcadbb695fdff3f1982b99a6cf32
Atikur Rahaman <atikurahad@gmail.com>
2023-03-14 23:34:17 +0600

    banner added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Home />
       <h1>Welcome To Ecoom</h1>
     </div>
   );
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+       <div><img src="https://chaldn.com/asset/Egg.ChaldalWeb.Fabric/Egg.ChaldalWeb1/1.0.0+Deploy-Release-227/Default/stores/chaldal/components/landingPage2/LandingPage/images/landingBannerTop.png" alt="" /></div>
+    </div>
+  );
+};
+
+export default Home;
